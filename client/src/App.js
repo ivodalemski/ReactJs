6ed0402af9346927aabb43235937510882b0a412
ivodalemski <ivodalemski7@gmail.com>
2023-03-29 09:18:16 +0300

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,23 @@
+import "./App.css";
+import Header from "./components/Header";
+import Post from "./components/Post";
+import { Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <Routes>
+      <Route
+        index
+        element={
+          <main>
+            <Header />
+            <Post />
+          </main>
+        }
+      />
+      <Route path={"/login"} element={<div>login</div>} />
+    </Routes>
+  );
+}
+
+export default App;
